39cc75eac7f601d583cf34abd7959c401b1d629d
IQBAL <46840757+iqbalagil@users.noreply.github.com>
2025-01-16 19:38:40 +0700

    Add project files.

---
+++ Dashboard/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using ReactiveUI;
+using System;
+using System.Reactive;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Input;
+
+namespace Tiket_Penerbangan_n_Kereta.Dashboard
+{
+
+    public class MainWindowViewModel : ReactiveObject
+    {
+        WindowState _windowState;
+        public ICommand MinimizeCommand { get; }
+        public ICommand MaximizeCommand { get; }
+        public ICommand ClosedCommand { get; }
+
+        public ICommand ColorDynamic { get; }
+
+        public MainWindowViewModel()
+        {
+            MinimizeCommand = ReactiveCommand.Create(() => WindowState = WindowState.Minimized);
+            MinimizeCommand = ReactiveCommand.Create(() => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized);
+            ClosedCommand = ReactiveCommand.Create(() => Environment.Exit(0));
+            ColorDynamic = ReactiveCommand.Create(() => DynamicColor());
+
+        }
+        public WindowState WindowState { get => _windowState;
+            set => this.RaiseAndSetIfChanged(ref _windowState, value); }
+
+        public void DynamicColor()
+        {
+            Application.Current.Resources["PrimaryColor"] = new SolidColorBrush(Color.FromRgb(251,251,251));
+            Application.Current.Resources["SecondayColor"] = new SolidColorBrush(Color.FromRgb(55, 140, 231));
+            Application.Current.Resources["PrimaryColor2"] = new SolidColorBrush(Color.FromRgb(5, 117, 237));
+            Application.Current.Resources["SecondaryColor2"] = new SolidColorBrush(Color.FromRgb(14, 16, 18));
+            Application.Current.Resources["DarkenColorBlue"] = new SolidColorBrush(Color.FromRgb(6, 102, 204));
+        }
+    }
+}
---
+++ Dashboard/UserInterface.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tiket_Penerbangan_n_Kereta;
+
+public partial class axa : Window
+{
+    public axa()
+    {
+        InitializeComponent();
+    }
+}
