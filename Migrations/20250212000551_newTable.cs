5f3e1c6374f414ebc0931afe493d308f2fc52001
IQBAL <46840757+iqbalagil@users.noreply.github.com>
2025-02-16 16:16:17 +0700

    update

---
+++ Migrations/20250212000551_newTable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tiket_Penerbangan_n_Kereta.Migrations
+{
+    /// <inheritdoc />
+    public partial class newTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tujuan",
+                table: "Pemesanan");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PemesananIdPemesanan",
+                table: "Rute",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rute_PemesananIdPemesanan",
+                table: "Rute",
+                column: "PemesananIdPemesanan");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rute_Pemesanan_PemesananIdPemesanan",
+                table: "Rute",
+                column: "PemesananIdPemesanan",
+                principalTable: "Pemesanan",
+                principalColumn: "IdPemesanan");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rute_Pemesanan_PemesananIdPemesanan",
+                table: "Rute");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Rute_PemesananIdPemesanan",
+                table: "Rute");
+
+            migrationBuilder.DropColumn(
+                name: "PemesananIdPemesanan",
+                table: "Rute");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tujuan",
+                table: "Pemesanan",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Migrations/20250214010055_new4Table.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tiket_Penerbangan_n_Kereta.Migrations
+{
+    /// <inheritdoc />
+    public partial class new4Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<char>(
+                name: "JenisKelamin",
+                table: "Penumpang",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "JenisKelamin",
+                table: "Penumpang",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(char),
+                oldType: "TEXT");
+        }
+    }
+}
